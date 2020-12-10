cabfc104c538961bd1a7eef76e2b9193573e0328
Thrasymachus01 <thrasymachus01@gmail.com>
2020-12-10 17:26:35 +0000

    Add README

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 ```
 git clone https://github.com/International-Financial-Exchange/interfinex-smart-contracts
 cd interfinex-smart-contracts
-mkdir ./.env.json
+npm install
+
+sudo npx hardhat compile
 ```
+
+To test:
+
+```
+sudo npx hardhat test
+```
+
+Expected: 2 failing tests due to `testAccrueInterest` not being a function. These tests pass if `testAccrueInterest` is manually uncommented in `MarginMarket.vy`.
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,7 @@ const { ethers } = require("ethers");
 const { task } = require("hardhat/config");
 require('dotenv').config();
 
-const projectDirs = JSON.parse(process.env.PROJECT_DIRS || []);
-
-console.log(projectDirs);
+const projectDirs = JSON.parse(process.env.PROJECT_DIRS || "[]");
 
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-vyper");
@@ -16,7 +14,6 @@ module.exports = {
     solidity: "0.7.3",
     vyper: {},
     abiExporter: {
-        // Can be commented out if not needed
         path: projectDirs,
         clear: true,
         flat: true,
@@ -25,18 +22,18 @@ module.exports = {
     networks: {
         ganache: {
             url: "http://localhost:7545",
-            accounts: [process.env.GANACHE_KEY]
+            accounts: process.env.GANACHE_KEY && [process.env.GANACHE_KEY]
         },
 
         kovan: {
             url: "https://kovan.infura.io/v3/f6a09cc8f51c45d2bd74137004115dbf",
-            accounts: [process.env.KOVAN_KEY],
+            accounts: process.env.KOVAN_KEY && [process.env.KOVAN_KEY],
             gasPrice: 1000000000,
         },
 
         mainnet: {
             url: "https://mainnet.infura.io/v3/f6a09cc8f51c45d2bd74137004115dbf",
-            accounts: [process.env.MAINNET_KEY],
+            accounts: process.env.MAINNET_KEY && [process.env.MAINNET_KEY],
             gasPrice: 60000000000,
         }
     },
